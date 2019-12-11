e10ebe6d4d10acc2da2d7b1823376276b6123bac
NadaO <lona_hotmail.com>
2019-12-11 10:49:29 +0300

    first commit

--- project_4/config/routes.rb
+++ project_4/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/products/:id/delete', to: "products#destroy"
   devise_for :users 
   resources :products
 
--- project_4/db/schema.rb
+++ project_4/db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2019_12_10_072622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "jobs", force: :cascade do |t|
-    t.string "title"
-    t.string "descripion"
-    t.string "location"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_jobs_on_user_id"
-  end
-
   create_table "products", force: :cascade do |t|
     t.string "productname"
     t.decimal "DanubPrice"
@@ -43,14 +33,9 @@ ActiveRecord::Schema.define(version: 2019_12_10_072622) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "firstname"
-    t.string "lastname"
-    t.string "phone"
-    t.date "dob"
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "jobs", "users"
 end
